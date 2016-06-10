6594b5403b7c2184b8f1371af7596d1590f712ad
Julianne D Johnson <jaydeejaye@gmail.com>
2016-06-10 13:03:27 -0400

    Added patient login path using alternate files. Plan to refactor and reuse login.html?

---
+++ SimplyHealth/public_html/js/loginPatient.js
@@ -0,0 +1,41 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function userLogin() {
+    var username = document.getElementById('inputUserName').value;
+    var pwd = document.getElementById('inputPassword').value;
+
+    var loginUserURL = "php/UserFunctions.php?username=" + username + "&password=" + pwd + "&action=login";
+    var result = false;
+    var message = "";
+    $.ajax({
+        url:loginUserURL,
+        async: false,
+        success: function (response) {
+            var json = JSON.parse(response);
+            if(json.success == 0) {
+                result = true;
+            }
+            else {
+                result = false;
+            }
+            message = json.message;
+        },
+        error: function () {
+            alert("Error: Unable to login the user!");
+            result = false;
+        }
+    });
+    alert(message);
+    if(result == true) {
+        location.href = "dashboardstaff.html?username=" + username;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+
